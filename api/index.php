c8a62952e74d26b0e62588a56836baf4810ad6a1
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:55:02 +0800

    test: autoloadt

--- api/index.php
+++ api/index.php
@@ -4,7 +4,10 @@ error_reporting(E_ALL);
 
 try {
     require __DIR__ . '/../vendor/autoload.php';
-    echo "Autoload OK!";
+    echo "Autoload OK!<br>";
+
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+    echo "Bootstrap OK!";
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
