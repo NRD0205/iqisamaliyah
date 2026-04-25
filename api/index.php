808ef16c7caf68234a12c111df82e2ea4610e4ca
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 23:01:20 +0800

    test: autoloadl

--- api/index.php
+++ api/index.php
@@ -3,19 +3,26 @@ ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
 try {
+    define('LARAVEL_START', microtime(true));
+
     require __DIR__ . '/../vendor/autoload.php';
 
     $app = require_once __DIR__ . '/../bootstrap/app.php';
     $app->useStoragePath('/tmp');
 
+    // Boot semua service providers
+    $app->bootstrapWith([
+        \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
+        \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
+        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+        \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
+        \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
+        \Illuminate\Foundation\Bootstrap\BootProviders::class,
+    ]);
+
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
     $request = Illuminate\Http\Request::capture();
-    echo "Request OK!<br>";
-
     $response = $kernel->handle($request);
-    echo "Handle OK!<br>";
-
     $response->send();
     $kernel->terminate($request, $response);
 } catch (Exception $e) {
