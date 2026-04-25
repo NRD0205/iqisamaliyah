27b864c8f90d4dcb1fd88cd9cfe8fcad4df0a1f2
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:50:32 +0800

    fix: restore laravel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,23 @@
 <?php
-echo "PHP is working!";
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+$_ENV['APP_STORAGE'] = '/tmp';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
