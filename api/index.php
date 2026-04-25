17a205732eb8a39ac83f39234b38f6910d40ef03
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:56:41 +0800

    test: autoloads

--- api/index.php
+++ api/index.php
@@ -7,7 +7,12 @@ try {
     echo "Autoload OK!<br>";
 
     $app = require_once __DIR__ . '/../bootstrap/app.php';
-    echo "Bootstrap OK!";
+    echo "Bootstrap OK!<br>";
+
+    $app->useStoragePath('/tmp');
+
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    echo "Kernel OK!";
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
