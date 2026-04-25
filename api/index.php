8d5a115e9749f8017d36865a52d33cdee641e2ea
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:53:04 +0800

    test: autoload

--- api/index.php
+++ api/index.php
@@ -1,23 +1,10 @@
 <?php
-
 ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$_ENV['APP_STORAGE'] = '/tmp';
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->useStoragePath('/tmp');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+try {
+    require __DIR__ . '/../vendor/autoload.php';
+    echo "Autoload OK!";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
