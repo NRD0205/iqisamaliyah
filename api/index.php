7b1b6f1a3761965cb68076d56395694eef3ae85c
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:22:19 +0800

    fix: hapus buildCommand, include vendor

--- api/index.php
+++ api/index.php
@@ -1,22 +1,16 @@
-<?php
-
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
-
-$_ENV['APP_STORAGE'] = '/tmp';
-
-define('LARAVEL_START', microtime(true));
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$app->useStoragePath('/tmp');
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+{
+"version": 2,
+"framework": null,
+"outputDirectory": "public",
+"functions": {
+"api/index.php": {
+"runtime": "vercel-php@0.7.2"
+}
+},
+"routes": [
+{
+"src": "/(.*)",
+"dest": "/api/index.php"
+}
+]
+}
