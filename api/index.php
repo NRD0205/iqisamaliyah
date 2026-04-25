6939ecca5d251b5378da8c6c9c4195f1c2b24e5c
RafifDzaki <dzakislebew521@gmail.com>
2026-04-25 22:45:05 +0800

    debug: tampilkan error detail

--- api/index.php
+++ api/index.php
@@ -1,16 +1,24 @@
-{
-"version": 2,
-"framework": null,
-"outputDirectory": "public",
-"functions": {
-"api/index.php": {
-"runtime": "vercel-php@0.7.2"
-}
-},
-"routes": [
-{
-"src": "/(.*)",
-"dest": "/api/index.php"
-}
-]
-}
+<?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Set storage path ke /tmp karena Vercel read-only
+$_ENV['APP_STORAGE'] = '/tmp';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp');
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
